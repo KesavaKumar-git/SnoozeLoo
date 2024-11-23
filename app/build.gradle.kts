53c20101d8dfde30d470eebccf862c0ded18daca
kesava <kesava.kumar@zohocorp.com>
2024-11-24 00:29:06 +0530

    Added Google fonts adaptive navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,13 +51,21 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+
     implementation(libs.androidx.material3)
+
     implementation(libs.androidx.core.splashscreen)
+
+    implementation(libs.androidx.ui.text.google.fonts)
+
+    implementation(libs.androidx.compose.material3.adaptive.navigation)
 
 
     testImplementation(libs.junit)
