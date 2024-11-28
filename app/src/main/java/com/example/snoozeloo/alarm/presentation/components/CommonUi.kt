6dab04e576f7f47f55c73fbfdc85aea93a3ea32c
kesava <kesava.kumar@zohocorp.com>
2024-11-28 09:23:19 +0530

    added default values for AlarmUI Added Common Alert dialog textField

--- app/src/main/java/com/example/snoozeloo/alarm/presentation/components/CommonUi.kt
+++ app/src/main/java/com/example/snoozeloo/alarm/presentation/components/CommonUi.kt
@@ -1,6 +1,8 @@
 package com.example.snoozeloo.alarm.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
@@ -10,20 +12,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.example.snoozeloo.alarm.presentation.models.Days
+import com.example.snoozeloo.ui.theme.SnoozeLooTheme
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun DaysChip(modifier: Modifier = Modifier, selectedMap: HashMap<Days, Boolean>)
+fun DaysChip(modifier: Modifier = Modifier, selectedMap: Set<Days>, onClick: (Days) -> Unit)
 {
     FlowRow(
         modifier = modifier
@@ -31,21 +44,18 @@ fun DaysChip(modifier: Modifier = Modifier, selectedMap: HashMap<Days, Boolean>)
         for (day in Days.entries)
         {
             AssistChip(
-                onClick = {},
+                onClick = { onClick(day) },
                 label = {
                     Text(
                         text = day.displayableText,
                         style = MaterialTheme.typography.labelMedium
                     )
                 },
-                enabled = selectedMap[day] == true,
                 shape = RoundedCornerShape(20.dp),
                 border = null,
                 colors = AssistChipDefaults.assistChipColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    labelColor = MaterialTheme.colorScheme.onPrimary,
-                    disabledContainerColor = MaterialTheme.colorScheme.secondary,
-                    disabledLabelColor = MaterialTheme.colorScheme.onSecondary
+                    containerColor = if (selectedMap.contains(day)) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary,
+                    labelColor = if (selectedMap.contains(day)) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondary,
                 )
             )
 
@@ -55,7 +65,7 @@ fun DaysChip(modifier: Modifier = Modifier, selectedMap: HashMap<Days, Boolean>)
 }
 
 @Composable
-fun LabelWithSwitch(label: String, isEnabled: Boolean, onChanged: (Boolean) -> Unit, modifier: Modifier = Modifier)
+fun LabelWithSwitch(label: String, isChecked: Boolean, onChanged: (Boolean) -> Unit, modifier: Modifier = Modifier)
 {
     Row (
         modifier = modifier.fillMaxWidth(),
@@ -72,7 +82,7 @@ fun LabelWithSwitch(label: String, isEnabled: Boolean, onChanged: (Boolean) -> U
         )
 
         Switch(
-            checked = isEnabled,
+            checked = isChecked,
             onCheckedChange = { onChanged(it) }
         )
     }
@@ -85,7 +95,9 @@ fun LabelValueCard(label: String, value: String, modifier: Modifier = Modifier)
         modifier = modifier.fillMaxWidth()
     ) {
         Row (
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -105,4 +117,84 @@ fun LabelValueCard(label: String, value: String, modifier: Modifier = Modifier)
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonAlertDialog(
+    title: String = "Alarm Name",
+    value: String,
+    primaryButton : String = "Save",
+    onConfirm : (String) -> Unit ={},
+    onDismiss: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    BasicAlertDialog(
+        onDismissRequest = onDismiss,
+        modifier = modifier.background(MaterialTheme.colorScheme.surfaceVariant, RoundedCornerShape(10.dp))
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier,
+                color = MaterialTheme.colorScheme.onSurface)
+
+            var alarmName by remember { mutableStateOf(value) }
+
+            EditTextField(
+                value = alarmName,
+                onValueChange = { alarmName = it }
+            )
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(
+                    onClick = { onConfirm(alarmName) }
+                ) {
+                    Text(
+                        text = primaryButton,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun EditTextField(value: String, onValueChange:(String) -> Unit, modifier: Modifier = Modifier)
+{
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        textStyle = MaterialTheme.typography.bodyLarge,
+        maxLines = 1
+    )
+}
+
+@PreviewLightDark
+@Composable
+fun DialogPreview(modifier: Modifier = Modifier)
+{
+    SnoozeLooTheme {
+        Scaffold { innerPadding ->
+            Column(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                CommonAlertDialog(value = "")
+            }
+        }
+    }
+    
 }
--- app/src/main/java/com/example/snoozeloo/alarm/presentation/models/AlarmUi.kt
+++ app/src/main/java/com/example/snoozeloo/alarm/presentation/models/AlarmUi.kt
@@ -1,15 +1,25 @@
 package com.example.snoozeloo.alarm.presentation.models
 
+import android.net.Uri
+import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 data class AlarmUi(
-    val alarmName: String,
-    val isActive: Boolean,
-    val time: ZonedDateTime,
-    val timeFormatted: String,
-    val isAM: Boolean,
-    val is24HrFormat: Boolean,
-    val selectedDays: HashMap<Days, Boolean>
+    val alarmName: String = "",
+    val isActive: Boolean = true,
+    val time: ZonedDateTime = ZonedDateTime.now(ZoneId.of("UTC")),
+    val shouldVibrate: Boolean = true,
+    val timeFormatted: String = formatZonedDateTimeToTime(time.withZoneSameInstant(ZoneId.systemDefault())),
+    val isAM: Boolean? =  time.withZoneSameInstant(ZoneId.systemDefault()).hour < 12,
+    val volume: Int = 80,
+    val selectedDays: MutableSet<Days> = mutableSetOf(),
+    val alarmRingtone: AlarmSound?
+)
+
+data class AlarmSound(
+    val title: String,
+    val uri: Uri
 )
 
 enum class Days(val displayableText: String)
@@ -21,4 +31,10 @@ enum class Days(val displayableText: String)
     Friday("Fr"),
     Saturday("Sa"),
     Sunday("Su")
+}
+
+fun formatZonedDateTimeToTime(zonedDateTime: ZonedDateTime): String
+{
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+    return zonedDateTime.format(formatter)
 }
