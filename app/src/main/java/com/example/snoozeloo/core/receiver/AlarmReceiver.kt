a30ea97d6b90b68f75eef5240448e5e0507b3141
kesava <kesava.kumar@zohocorp.com>
2024-11-30 15:40:54 +0530

    Alarm and notificationAction receiver added.

---
+++ app/src/main/java/com/example/snoozeloo/core/receiver/AlarmReceiver.kt
@@ -0,0 +1,70 @@
+package com.example.snoozeloo.core.receiver
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.snoozeloo.R
+import com.example.snoozeloo.alarm.presentation.alarm_snooze.SnoozeActivity
+import com.example.snoozeloo.core.constants.StringConstants
+import com.example.snoozeloo.core.database.SnoozeLooDatabase
+import com.example.snoozeloo.core.domain.utils.NotificationUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AlarmReceiver: BroadcastReceiver()
+{
+    override fun onReceive(context: Context?, intent: Intent?)
+    {
+        CoroutineScope(Dispatchers.IO).launch {
+
+            if (context != null)
+            {
+                intent?.getIntExtra("alarm_id", -1)?.let {
+
+                    if (it != -1 && SnoozeLooDatabase.getInstance(context).alarmsDao().isAlarmActive(it))
+                    {
+                        val snoozeScreen = Intent(context, SnoozeActivity::class.java)
+                        snoozeScreen.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        snoozeScreen.putExtra("alarm_id", it)
+
+                        context.startActivity(snoozeScreen)
+
+                        postNotification(context, it, intent.getStringExtra("alarm_time")?: "")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun postNotification(context: Context, alarmId: Int, alarmTime: String)
+    {
+        val notificationChannel = NotificationUtil.buildNotificationChannel(
+            id = StringConstants.ALARM_CHANNEL_ID,
+            channelName = context.getString(R.string.mt_alarm_channel_name),
+            importance = NotificationManager.IMPORTANCE_LOW,
+            visibility = Notification.VISIBILITY_PUBLIC)
+
+        NotificationUtil.createChannel(context, notificationChannel)
+
+        val actionIntent = Intent(context, NotificationActionReceiver::class.java)
+        actionIntent.putExtra("alarm_id", alarmId)
+        actionIntent.putExtra("alarm_time", alarmTime)
+
+        val pendingIntent1 = PendingIntent.getActivity(context, 1, actionIntent.putExtra("notify_action", "remind_later"), PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+        val pendingIntent2 = PendingIntent.getActivity(context, 2, actionIntent.putExtra("notify_action", "turn_off"), PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
+        val notification = NotificationUtil.createAlarmNotification(
+            context = context,
+            content = alarmTime,
+            smallIcon = R.drawable.ic_alarm,
+            action1 = pendingIntent1,
+            action2 = pendingIntent2)
+
+        NotificationUtil.postNotification(context, 1, notification)
+    }
+}
---
+++ app/src/main/java/com/example/snoozeloo/core/receiver/NotificationActionReceiver.kt
@@ -0,0 +1,30 @@
+package com.example.snoozeloo.core.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.snoozeloo.core.domain.utils.AlarmConfigureManager
+import com.example.snoozeloo.core.presentation.utils.RingtoneManagerUtil
+
+class NotificationActionReceiver: BroadcastReceiver()
+{
+    override fun onReceive(context: Context?, intent: Intent?)
+    {
+        if (context != null)
+        {
+            intent?.getIntExtra("alarm_id", -1)?.let {
+
+                intent.getStringExtra("notify_action").let { action ->
+                    if (action == "remind_later")
+                    {
+                        AlarmConfigureManager.snoozeFor5Min(context = context, id = it, label = intent.getStringExtra("alarm_time")?: "")
+                    }
+                    else if (action == "turn_off")
+                    {
+                        RingtoneManagerUtil.stopRingtone()
+                    }
+                }
+            }
+        }
+    }
+}
